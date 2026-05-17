8d0f2fb6d6db4ed943d1a1c925b677c2341a91a0
rahmaAshraf <rahmafadl087@gmail.com>
2026-05-18 01:21:56 +0300

    Conditionally require dotenv based on NODE_ENV
    Load environment variables conditionally based on NODE_ENV.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const app = require('./src/app');
 const connectDatabase = require('./src/config/database');
 
